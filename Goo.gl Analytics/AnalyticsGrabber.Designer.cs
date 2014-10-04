384d146a048eaacf6589aee06d3b7ea5de470849
Jacob Soo <jacobsoo@users.noreply.github.com>
2014-10-04 23:21:43 +0800

    Initial commit

---
+++ Goo.gl Analytics/AnalyticsGrabber.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Goo.gl_Analytics
+{
+    partial class AnalyticsGrabber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGrab = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGrab
+            // 
+            this.btnGrab.Location = new System.Drawing.Point(289, 15);
+            this.btnGrab.Name = "btnGrab";
+            this.btnGrab.Size = new System.Drawing.Size(84, 23);
+            this.btnGrab.TabIndex = 0;
+            this.btnGrab.Text = "Grab Analytics";
+            this.btnGrab.UseVisualStyleBackColor = true;
+            this.btnGrab.Click += new System.EventHandler(this.btnGrab_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "goo.gl URL : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Results : ";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(89, 18);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(179, 20);
+            this.txtURL.TabIndex = 3;
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(16, 78);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(357, 133);
+            this.txtResults.TabIndex = 4;
+            // 
+            // AnalyticsGrabber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 226);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGrab);
+            this.Name = "AnalyticsGrabber";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Goo.gl Analytics";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGrab;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.TextBox txtResults;
+    }
+}
+
---
+++ Goo.gl Analytics/AnalyticsGrabber.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace Goo.gl_Analytics
+{
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public partial class AnalyticsGrabber : Form
+    {
+        public static MSScriptControl.ScriptControl sc = new MSScriptControl.ScriptControl();
+        public AnalyticsGrabber()
+        {
+            InitializeComponent();
+        }
+
+        private string FindScript()
+        {
+            string fpath = "";
+            string base_path = AppDomain.CurrentDomain.BaseDirectory;
+            for (int i = 0; i < 4; i++)
+            {
+                fpath = base_path + "\\beautify.js";
+                if (!File.Exists(fpath)) base_path += "\\..";
+                else break;
+            }
+            if (!File.Exists(fpath)) fpath = "";
+            return fpath;
+        }
+
+        private void btnGrab_Click(object sender, EventArgs e)
+        {
+            string szShortURL = txtURL.Text;
+            string szBase = "https://www.googleapis.com/urlshortener/v1/url?shortUrl=";
+            string szPath = "";
+            string szResults = "";
+            if (szShortURL.Contains("http://"))
+            {
+                szPath = szBase + szShortURL + "&projection=FULL";
+            }
+            else
+            {
+                szPath = szBase + "http://" + szShortURL + "&projection=FULL";
+            }
+            using (WebClient client = new WebClient())
+            {
+                szResults = client.DownloadString(szPath);
+                txtResults.Text = szResults;
+
+                sc.Language = "jScript";
+                sc.AddObject("txtResults", txtResults, true);
+
+                string fpath = FindScript();
+                if (fpath.Length == 0)
+                {
+                    MessageBox.Show("Could not locate beautifer.js");
+                    return;
+                }
+
+                String script = File.ReadAllText(fpath);
+                sc.AddCode(script);
+                sc.AddCode(@"txtResults.Text = js_beautify(txtResults.Text, {indent_size: 4, indent_char: ' '}).split('\n').join('\r\n');");
+            }
+        }
+    }
+}
